a6352c8f53fa3002d274479bf9850a0155bb94b6
Jing He <jinghe@Jings-MacBook-Pro-2.local>
2024-11-10 18:29:06 -0500

    js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    var sideMenu = document.getElementById("side-menu");
+    sideMenu.classList.toggle("hidden"); // Toggles the 'hidden' class on or off
+});
